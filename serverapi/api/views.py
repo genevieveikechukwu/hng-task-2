7bd31f224fe473b10092a5b7a123816cd3aba7fc
Genevieve Ikechukwu <ikechukwummachukwu2020@gmail.com>
2022-11-05 07:44:10 -0700

    fixed some errors

--- serverapi/api/views.py
+++ serverapi/api/views.py
@@ -1,95 +1,53 @@
 import re
-from rest_framework.parsers import JSONParser
-from rest_framework import status, response
-from rest_framework.decorators import parser_classes
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from django.http import JsonResponse
-from .models import Calculatorapi
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.generics import GenericAPIView
-from rest_framework import generics
-from .serializers import CalculatorInputSerializer
-#   if request.method == 'POST':
-#       return JsonResponse({"message": "Got some data!", "data": JSONParser().parse(request.data)})
-#   return Response({"message": "Hello, world!"})
 
-@api_view(["GET"])
-def home(request, *args, **kwargs):
-  header = {"Access-Control-Allow-Origin":"*"}
-  data = {
-    "slackUsername":"Genevieve",
-    "backend":True,
-    "age": 18,
-    "bio":"your bio"
-  }
-  return JsonResponse(data, headers=header)
+def calculate(operation_type):
 
-bad_chars = ['?', '!', '@', '.', '#', '%', '&' ',' ']', '[', '[]', ]    
-operation_add = ['add', 'addition', 'increment', '+', 'sum']
-operation_sub = ['subtract', 'minus', 'remove', '-']
-operation_multiply = ['multiply', 'multiplicattion', 'product', '*']
+    bad_chars = ['?', '!', '@', '.', '#', '%', '&' ',' ']', '[', '[]', ]    
 
-class CalculateView(GenericAPIView):
-    serializer_class = CalculatorInputSerializer
+    for i in bad_chars:
+        operation_type = operation_type.replace(i, '')
+    # print(type(operation_type))
+    main_sentence = operation_type.split()
+    print(main_sentence)
 
-    def post(self, request, *args, **kwargs):
-        header = {
-          "Access-Control-Allow-Origin":"*"
-        }
-        operation_type = self.request.data['operation_type']
-        for i in bad_chars:
-            operation_type = operation_type.replace(i, '')
-        operation_sentence_list = self.request.data['operation_type'].split()
-        if(self.request.data['x']) and (self.request.data['y']) == '':
-            regex = '\d+'
-            match = re.findall(regex, operation_type)
-            new = match
-            self.request.data['x'] = ''.join(map(str, new[0]))
-            self.request.data['y'] = ''.join(map(str, new[1]))
-            # print(x, y)
-        x = self.request.data['x']
-        y = self.request.data['y']
-        check1 = any(item in operation_add for item in operation_sentence_list)
-        check2 = any(item in operation_sub for item in operation_sentence_list)
-        check3 = any(item in operation_multiply for item in operation_sentence_list)
-        result = ''
-        if check1 is True:
-           result = float(x) + float(y)
-           operation_type = 'Addition'
-          #  print(result)
-        elif check2 is True:
-           result = float(x) - float(y)
-           operation_type = 'Subtraction'
-        elif check3 is True:
-           result = float(x) * float(y)
-           operation_type = 'Multiplication'
-        else:
-            return response.Response({ 'error':'invalid operator'}, status=status.HTTP_400_BAD_REQUEST, headers = header) 
-        return response.Response({ "slackUsername": 'Genevieve_', 'operation_type':operation_type , 'result':result}, status=status.HTTP_200_OK, headers = header)
+# To print only a num
+    regex = '\d+'
+    match = re.findall(regex, operation_type)
+    print(match)
+    new = match
+# convert to str
+    a = ''.join(map(str, new[0]))
+    b = ''. join(map(str, new[1]))
+    # # convert to float
+    x = float(a)
+    y = float(b)
+    print(x)
+    print(y)
 
 
+    operation_add = ['add', 'addition', 'increment', '+']
+    operation_sub = ['subtract', 'minus', 'remove', '-']
+    operation_multiply = ['multiply', 'multiplicattion', 'product', '*']
+    check1 = any(item in operation_add for item in main_sentence)
+    check2 = any(item in operation_sub for item in main_sentence)
+    check3 = any(item in operation_multiply for item in main_sentence)
 
+    result = ''
+    if check1 is True:
+      print( x + y)
+      result = 'Addition'
+      print(result)
+    elif check2 is True:
+        print( x - y)
+        result = 'Subtraction'
+    elif check3 is True:
+        result = 'Multiplication'
+        print( x * y)
 
-# class CreateView(generics.CreateAPIView):
+#     print('not available')
 
-#   serializer_class = CalculatorInputSerializer
+operation_type = 'What is the product of 6 and 5?'
 
-#   def create(self, request, *args, **kwargs):
-#     serializer = CalculatorInputSerializer(data=request.POST)
-#     serializer.is_valid(raise_exception=True)
-#     self.perform_create(serializer)
-#     headers = self.get_success_headers(serializer.data)
-#     response = CalculatorOutputSerializer(Calculatorapi).data
-#     return Response(response, status=status.HTTP_201_CREATED, headers=headers)
+calculate(operation_type)
 
-# class RetrieveView(generics.RetrieveAPIView):
-#   serializer_class = CalculatorOutputSerializer
-
-#   def retrieve(self, request, *args, **kwargs):
-
-#        serializer = CalculatorOutputSerializer(data=request.GET)
-#        return Response(request.data, status=status.HTTP_200_OK)
-
-
+    
